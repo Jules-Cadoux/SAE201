968bb80706f6bb959227deebe6ea177d0aa9fe35
Jules Cadoux-Jacquet <Jules.Cadoux-Jacquet@etu.univ-smb.fr>
2025-06-05 15:16:59 +0200

    Ajout Commande, Employe, Role

---
+++ SAE201/Model/Commande.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE201.Model
+{
+    public class Commande
+    {
+        private int numCommande;
+        private DateTime dateCommande;
+        private bool valider;
+        private double prixTotal;
+
+        public int NumCommande
+        {
+            get
+            {
+                return numCommande;
+            }
+
+            set
+            {
+                numCommande = value;
+            }
+        }
+
+        public DateTime DateCommande
+        {
+            get
+            {
+                return dateCommande;
+            }
+
+            set
+            {
+                dateCommande = value;
+            }
+        }
+
+        public bool Valider
+        {
+            get
+            {
+                return valider;
+            }
+
+            set
+            {
+                valider = value;
+            }
+        }
+
+        public double PrixTotal
+        {
+            get
+            {
+                return this.prixTotal;
+            }
+
+            set
+            {
+                this.prixTotal = value;
+            }
+        }
+    }
+}
--- SAE201/Model/Employe.cs
+++ SAE201/Model/Employe.cs
@@ -6,7 +6,78 @@ using System.Threading.Tasks;
 
 namespace SAE201.Model
 {
-    internal class Employe
+    public class Employe
     {
+        private int numEmploye;
+        private string nom;
+        private string prenom;
+        private string login;
+        private string mdp;
+
+        public int NumEmploye
+        {
+            get
+            {
+                return this.numEmploye;
+            }
+
+            set
+            {
+                this.numEmploye = value;
+            }
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return nom;
+            }
+
+            set
+            {
+                nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return prenom;
+            }
+
+            set
+            {
+                prenom = value;
+            }
+        }
+
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+
+            set
+            {
+                login = value;
+            }
+        }
+
+        public string Mdp
+        {
+            get
+            {
+                return this.mdp;
+            }
+
+            set
+            {
+                this.mdp = value;
+            }
+        }
+
     }
 }
