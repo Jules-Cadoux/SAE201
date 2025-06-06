9cef38c09e3035a8a1348cd0b8defc1d9e1d246b
Jules Cadoux-Jacquet <Jules.Cadoux-Jacquet@etu.univ-smb.fr>
2025-06-06 08:49:27 +0200

    2

--- SAE201/Model/Commande.cs
+++ SAE201/Model/Commande.cs
@@ -18,8 +18,9 @@ namespace SAE201.Model
         {
         }
 
-        public Commande(int numCommande, Employe numEmploye, DateTime dateCommande, bool valider, double prixTotal) : this(numCommande)
+        public Commande(int numCommande, Employe numEmploye, DateTime dateCommande, bool valider, double prixTotal)
         {
+            this.NumCommande = numCommande;
             this.NumEmploye = numEmploye;
             this.DateCommande = dateCommande;
             this.Valider = valider;
