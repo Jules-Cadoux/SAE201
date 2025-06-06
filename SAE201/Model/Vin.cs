3c69e98ec112422f8a449383681e9d14c166a448
Hugo Allix <Hugo.Allix@etu.univ-smb.fr>
2025-06-06 08:50:03 +0200

    3 classes

--- SAE201/Model/Vin.cs
+++ SAE201/Model/Vin.cs
@@ -10,19 +10,35 @@ namespace SAE201.Model
     public class Vin
     {
         private int numVin;
+        private Fournisseur numFournisseur;
+        private TypeVin numType;
+        private Appelation numType2;
         private string nomVin;
         private double prixVin;
         private string description;
         private int millésime;
 
-        public Vin(int numVin, Fournisseur numFournisseur, string nomVin, double prixVin, string description, int millésime)
+        public Vin(int numVin, Fournisseur numFournisseur, TypeVin numType, Appelation numType2, string nomVin, double prixVin, string description, int millésime)
         {
             this.NumVin = numVin;
+            this.NumFournisseur = numFournisseur;
+            this.NumType = numType;
+            this.NumType2 = numType2;
             this.NomVin = nomVin;
             this.PrixVin = prixVin;
             this.Description = description;
             this.Millésime = millésime;
         }
+
+        public Vin(int numVin, Fournisseur numFournisseur, TypeVin numType, Appelation numType2)
+        {
+            this.NumVin = numVin;
+            this.NumFournisseur = numFournisseur;
+            this.NumType = numType;
+            this.NumType2 = numType2;
+        }
+
+        public Vin() { }
 
         public int NumVin
         {
@@ -34,6 +50,45 @@ namespace SAE201.Model
             set
             {
                 numVin = value;
+            }
+        }
+
+        public Fournisseur NumFournisseur
+        {
+            get
+            {
+                return this.numFournisseur;
+            }
+
+            set
+            {
+                this.numFournisseur = value;
+            }
+        }
+
+        public TypeVin NumType
+        {
+            get
+            {
+                return this.numType;
+            }
+
+            set
+            {
+                this.numType = value;
+            }
+        }
+
+        public Appelation NumType2
+        {
+            get
+            {
+                return this.numType2;
+            }
+
+            set
+            {
+                this.numType2 = value;
             }
         }
 
@@ -61,8 +116,7 @@ namespace SAE201.Model
             {
                 if(value <= 0)
                 {
-                    MessageBox.Show("Le prix en peut pas être <= à la date actuelle", "Erreur création vin", MessageBoxButton.OK, MessageBoxImage.Error);
-
+                    MessageBox.Show("Le prix en peut pas être <= 0", "Erreur création vin", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
                 prixVin = value;
             }
@@ -97,6 +151,8 @@ namespace SAE201.Model
                 this.millésime = value;
             }
         }
+
+
 
         public override bool Equals(object? obj)
         {
